cd3763fb96ccdb4b5bdeb48661c802b4727d495d
Brad <mirado.akemi@gmail.com>
2024-04-09 21:03:39 +0200

    main changed

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: non_constant_identifier_names, unused_field
+
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/home.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
